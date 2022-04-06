618a0ff7ac3c51ef44fce94e896175f9ce893a3a
mjk-yoshida <yoshida.k@mjk.co.jp>
2022-04-06 23:51:53 +0900

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { products } from '../products';
 
 @Component({
   selector: 'app-root',
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,19 +5,17 @@ import { products } from '../products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  products = products;
+  student_name: any = '太郎';
+  products = '太郎';
 
   share() {
     window.alert('The product has been shared!');
   }
+
+  notify() {
+    window.alert('お知らせを受け取ります！');
+  }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
